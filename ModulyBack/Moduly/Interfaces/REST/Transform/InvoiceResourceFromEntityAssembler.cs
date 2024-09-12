79164c6bf2da0f7d4e0faa1b0c2cf0bae1f7a948
APOgamer <48342953+APOgamer@users.noreply.github.com>
2024-09-12 00:43:10 -0500

    type fixes

--- ModulyBack/Moduly/Interfaces/REST/Transform/InvoiceResourceFromEntityAssembler.cs
+++ ModulyBack/Moduly/Interfaces/REST/Transform/InvoiceResourceFromEntityAssembler.cs
@@ -1,25 +1,30 @@
 ﻿using ModulyBack.Moduly.Domain.Model.Entities;
 using ModulyBack.Moduly.Interfaces.REST.Resources;
 
-namespace ModulyBack.Moduly.Interfaces.REST.Transform;
-
-public static class InvoiceResourceFromEntityAssembler
+namespace ModulyBack.Moduly.Interfaces.REST.Transform
 {
-    public static InvoiceResource ToResourceFromEntity(Invoice invoice)
+    public static class InvoiceResourceFromEntityAssembler
     {
-        return new InvoiceResource
+        public static InvoiceResource ToResourceFromEntity(Invoice invoice)
         {
-            Id = invoice.Id,
-            Code = invoice.Code,
-            ModuleId = invoice.ModuleId,
-            IssuerId = invoice.IssuerId,
-            IssueDate = invoice.IssueDate,
-            DueDate = invoice.DueDate,
-            Description = invoice.Description,
-            Quantity = invoice.Quantity,
-            UnitPrice = invoice.UnitPrice,
-            TotalPayment = invoice.TotalPayment,
-            Status = invoice.Status
-        };
+            return new InvoiceResource
+            {
+                Id = invoice.Id,
+                Code = invoice.Code,
+                ModuleId = invoice.ModuleId,
+                IssuerId = invoice.IssuerId,
+                IssueDate = invoice.IssueDate,
+                DueDate = invoice.DueDate,
+                Description = invoice.Description,
+                Quantity = invoice.Quantity,
+                UnitPrice = invoice.UnitPrice, // Cambio: puede ser nulo
+                TotalPayment = invoice.TotalPayment,
+                Status = invoice.Status,
+                Currency = invoice.Currency, // Nuevo campo
+                ExchangeRate = invoice.ExchangeRate, // Nuevo campo
+                DiscountDate = invoice.DiscountDate, // Nuevo campo
+                TCEA = invoice.TCEA // Nuevo campo
+            };
+        }
     }
 }
